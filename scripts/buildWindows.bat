a7ef7e9e17e7c4a8f52368f46fcf75770fee72dc
TheKodeToad <57493648+TheKodeToad@users.noreply.github.com>
2021-10-05 11:13:42 +0100

    I can't think of a message

--- scripts/buildWindows.bat
+++ scripts/buildWindows.bat
@@ -4,5 +4,5 @@ cd game
 gradlew setupCIWorkspace || exit 1
 gradlew build || exit  1
 cd ..
-electron-packager . sol-client-launcher --platform windows --arch x64 --out dist/ || exit 1
-node scripts/buildWindows.js || exit 1
+electron-packager . sol-client-launcher --platform windows --arch x64 --out dist\ || exit 1
+node scripts\buildWindows.js || exit 1

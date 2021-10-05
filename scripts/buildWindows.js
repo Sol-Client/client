775edc1adb92eb349c32bf1494153ea951dea9cc
TheKodeToad <57493648+TheKodeToad@users.noreply.github.com>
2021-10-05 14:21:01 +0100

    Show error and change output directory

--- scripts/buildWindows.js
+++ scripts/buildWindows.js
@@ -1,11 +1,20 @@
 const electronInstaller = require("electron-winstaller");
+const process = require("process");
+
 async function build() {
-	await electronInstaller.createWindowsInstaller({
-		appDirectory: "dist/sol-client-launcher-x64/",
-		outDirectory: "dist/installers/",
-		authors: "mcblueparrot",
-		exe: "Sol Client Setup.exe"
-	});
+	console.log("Building installer...");
+	try {
+		await electronInstaller.createWindowsInstaller({
+			appDirectory: "dist\sol-client-launcher-x64",
+			outDirectory: "dist\installers",
+			authors: "mcblueparrot",
+			exe: "Sol Client Setup.exe"
+		});
+	}
+	catch(error) {
+		console.error(error.message);
+		process.exit(1);
+	}
 }
 
 build();

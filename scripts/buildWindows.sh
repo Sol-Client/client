1f1a3afe6bf562042058d813dfd37a040e2ba008
TheKodeToad <57493648+TheKodeToad@users.noreply.github.com>
2021-10-05 11:08:18 +0100

    More oversights Sorry about the commit history, I have to test everything on GitHub actions.

---
+++ scripts/buildWindows.sh
@@ -0,0 +1,8 @@
+@echo off
+
+cd game
+gradlew setupCIWorkspace || exit 1
+gradlew build || exit  1
+cd ..
+electron-packager . sol-client-launcher --platform windows --arch x64 --out dist/ || exit 1
+node scripts\\buildWindows.js || exit 1

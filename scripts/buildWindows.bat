bfc0cabd224890a317ceb825230194c47d16f38d
TheKodeToad <57493648+TheKodeToad@users.noreply.github.com>
2021-10-05 12:10:22 +0100

    Remove exit

--- scripts/buildWindows.bat
+++ scripts/buildWindows.bat
@@ -1,8 +1,8 @@
-@echo on
+@echo off
 
 cd game
-gradlew setupCIWorkspace || exit 1
-gradlew build || exit  1
+gradlew setupCIWorkspace
+gradlew build
 cd ..
-electron-packager . sol-client-launcher --platform windows --arch x64 --out dist\ || exit 1
-node scripts\buildWindows.js || exit 1
+electron-packager . sol-client-launcher --platform windows --arch x64 --out dist\
+node scripts\buildWindows.js

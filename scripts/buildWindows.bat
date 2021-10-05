08e81ca2649a2355df75cf0f9c8085d20a39ec8c
TheKodeToad <57493648+TheKodeToad@users.noreply.github.com>
2021-10-05 12:14:20 +0100

    Add more information

--- scripts/buildWindows.bat
+++ scripts/buildWindows.bat
@@ -1,8 +1,13 @@
-@echo off
+@echo on
 
 cd game
+
+echo Setting up CI Workspace...
 gradlew setupCIWorkspace
+echo Building game...
 gradlew build
 cd ..
-electron-packager . sol-client-launcher --platform windows --arch x64 --out dist\
+
+echo Packaging launcher...
+electron-packager . sol-client-launcher --platform windows --arch x64 --out dist
 node scripts\buildWindows.js

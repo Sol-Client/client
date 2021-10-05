750c0ae0e9ae2110dea972a2ecfaf9091db1f9d7
TheKodeToad <57493648+TheKodeToad@users.noreply.github.com>
2021-10-05 14:17:55 +0100

    No need to specify arch

--- scripts/buildLinux.sh
+++ scripts/buildLinux.sh
@@ -3,4 +3,4 @@ cd game
 ./gradlew build || exit  1
 cd ..
 electron-packager . sol-client-launcher --platform linux --arch x64 --out dist/ || exit 1
-electron-installer-debian --src dist/sol-client-launcher-linux-x64/ --dest dist/installers/ --arch amd64 || exit 1
+electron-installer-debian --src dist/sol-client-launcher-linux-x64/ --dest dist/installers/ || exit 1

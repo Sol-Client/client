c2f6923054fa0fdcef07be377c8083211a6bc2dc
TheKodeToad <57493648+TheKodeToad@users.noreply.github.com>
2022-01-01 07:42:57 +0000

    Remove symlink I was using for testing

--- game/src/main/java/me/mcblueparrot/client/util/Utils.java
+++ game/src/main/java/me/mcblueparrot/client/util/Utils.java
@@ -171,20 +171,20 @@ public class Utils {
 	/*
 	 * Single following method:
 	 *
-	 *	   Copyright (C) 2018-present Hyperium <https://hyperium.cc/>
+	 *       Copyright (C) 2018-present Hyperium <https://hyperium.cc/>
 	 *
-	 *	   This program is free software: you can redistribute it and/or modify
-	 *	   it under the terms of the GNU Lesser General Public License as published
-	 *	   by the Free Software Foundation, either version 3 of the License, or
-	 *	   (at your option) any later version.
+	 *       This program is free software: you can redistribute it and/or modify
+	 *       it under the terms of the GNU Lesser General Public License as published
+	 *       by the Free Software Foundation, either version 3 of the License, or
+	 *       (at your option) any later version.
 	 *
-	 *	   This program is distributed in the hope that it will be useful,
-	 *	   but WITHOUT ANY WARRANTY; without even the implied warranty of
-	 *	   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	 *	   GNU Lesser General Public License for more details.
+	 *       This program is distributed in the hope that it will be useful,
+	 *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 *       GNU Lesser General Public License for more details.
 	 *
-	 *	   You should have received a copy of the GNU Lesser General Public License
-	 *	   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	 *       You should have received a copy of the GNU Lesser General Public License
+	 *       along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	 */
 	public void drawCircle(float xx, float yy, int radius, int col) {
 		float f = (col >> 24 & 0xFF) / 255.0F;

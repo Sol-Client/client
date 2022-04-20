7fdc86e99ae73277d941942c897940811d355823
TheKodeToad <57493648+TheKodeToad@users.noreply.github.com>
2022-04-20 09:16:40 +0100

    Slightly Better 1.7 Visuals

--- game/src/main/java/me/mcblueparrot/client/mod/impl/V1_7VisualsMod.java
+++ game/src/main/java/me/mcblueparrot/client/mod/impl/V1_7VisualsMod.java
@@ -8,12 +8,17 @@ package me.mcblueparrot.client.mod.impl;
 import com.google.gson.annotations.Expose;
 
 import me.mcblueparrot.client.event.EventHandler;
+import me.mcblueparrot.client.event.impl.PreRenderTickEvent;
+import me.mcblueparrot.client.event.impl.PreTickEvent;
 import me.mcblueparrot.client.event.impl.TransformFirstPersonItemEvent;
 import me.mcblueparrot.client.mod.Mod;
 import me.mcblueparrot.client.mod.ModCategory;
 import me.mcblueparrot.client.mod.annotation.Option;
+import me.mcblueparrot.client.util.access.AccessEntityLivingBase;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemFishingRod;
+import net.minecraft.util.MovingObjectPosition;
 
 public class V1_7VisualsMod extends Mod {
 
@@ -22,7 +27,10 @@ public class V1_7VisualsMod extends Mod {
 	public boolean useAndMine = true;
 	@Expose
 	@Option
-	public boolean rod = true;
+	private boolean particles = true;
+	@Expose
+	@Option
+	public boolean items = true;
 	@Expose
 	@Option
 	public boolean armourDamage = true;
@@ -67,9 +75,40 @@ public class V1_7VisualsMod extends Mod {
 	}
 
 	@EventHandler
+	public void onTick(PreTickEvent event) {
+		if(mc.thePlayer != null && mc.thePlayer.capabilities.allowEdit
+				&& isEnabled() && useAndMine && mc.objectMouseOver != null
+				&& mc.objectMouseOver.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK
+				&& mc.thePlayer != null
+				&& mc.gameSettings.keyBindAttack.isKeyDown() && mc.gameSettings.keyBindUseItem.isKeyDown()
+				&& mc.thePlayer.getItemInUseCount() > 0) {
+			if((!mc.thePlayer.isSwingInProgress
+				|| mc.thePlayer.swingProgressInt >= ((AccessEntityLivingBase) mc.thePlayer).accessArmSwingAnimationEnd()
+				/ 2 || mc.thePlayer.swingProgressInt < 0)) {
+				mc.thePlayer.swingProgressInt = -1;
+				mc.thePlayer.isSwingInProgress = true;
+			}
+
+			if(particles) {
+				mc.effectRenderer.addBlockHitEffects(mc.objectMouseOver.getBlockPos(), mc.objectMouseOver.sideHit);
+			}
+		}
+	}
+
+	@EventHandler
 	public void onItemTransform(TransformFirstPersonItemEvent event) {
-		if(event.itemToRender.getItem() instanceof ItemFishingRod && rod) {
-			GlStateManager.translate(-0.16F, 0.1F, 0);
+		if(!items) {
+			return;
+		}
+
+		// https://github.com/sp614x/optifine/issues/2098
+
+		if(mc.thePlayer.isUsingItem() && event.itemToRender.getItem() instanceof ItemBow) {
+			GlStateManager.translate(-0.01f, 0.05f, -0.06f);
+		}
+		else if(event.itemToRender.getItem() instanceof ItemFishingRod) {
+			GlStateManager.translate(0.08f, -0.027f, -0.33f);
+			GlStateManager.scale(0.93f, 1.0f, 1.0f);
 		}
 	}
 

247e4fa8efe96141e172505ac957274119f21b9b
TheKodeToad <57493648+TheKodeToad@users.noreply.github.com>
2022-05-20 10:14:14 +0100

    Fix shader resizing once and for all

--- game/src/main/java/me/mcblueparrot/client/mixin/client/MixinEntityRenderer.java
+++ game/src/main/java/me/mcblueparrot/client/mixin/client/MixinEntityRenderer.java
@@ -25,6 +25,7 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.settings.GameSettings;
 import net.minecraft.client.shader.ShaderGroup;
+import net.minecraft.client.shader.ShaderLinkHelper;
 import net.minecraft.entity.Entity;
 
 @Mixin(EntityRenderer.class)
@@ -50,15 +51,13 @@ public abstract class MixinEntityRenderer {
 
 	@Inject(method = "updateShaderGroupSize", at = @At("RETURN"))
 	public void updateShaders(int width, int height, CallbackInfo callback) {
-		if(mc.theWorld == null) {
+		if(ShaderLinkHelper.getStaticShaderLinkHelper() == null || !OpenGlHelper.shadersSupported) {
 			return;
 		}
 
-		if(OpenGlHelper.shadersSupported) {
-			for (ShaderGroup group : Client.INSTANCE.bus.post(new PostProcessingEvent(PostProcessingEvent.Type.UPDATE))
-					.groups) {
-				group.createBindFramebuffers(width, height);
-			}
+		for(ShaderGroup group : Client.INSTANCE.bus.post(new PostProcessingEvent(PostProcessingEvent.Type.UPDATE))
+				.groups) {
+			group.createBindFramebuffers(width, height);
 		}
 	}
 

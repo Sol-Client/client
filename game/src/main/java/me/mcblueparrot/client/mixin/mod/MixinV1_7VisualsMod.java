8e72fd6a36116ad4dd09efa16e041ad56d215825
TheKodeToad <57493648+TheKodeToad@users.noreply.github.com>
2022-05-24 11:48:27 +0100

    Almost perfect eating animation I will have some very weird dreams tonight...

--- game/src/main/java/me/mcblueparrot/client/mixin/mod/MixinV1_7VisualsMod.java
+++ game/src/main/java/me/mcblueparrot/client/mixin/mod/MixinV1_7VisualsMod.java
@@ -1,5 +1,7 @@
 package me.mcblueparrot.client.mixin.mod;
 
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -13,24 +15,21 @@ import me.mcblueparrot.client.mod.impl.V1_7VisualsMod;
 import me.mcblueparrot.client.util.access.AccessEntityLivingBase;
 import me.mcblueparrot.client.util.access.AccessMinecraft;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.client.renderer.entity.layers.LayerArmorBase;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
 import net.minecraft.util.MovingObjectPosition;
 
 public abstract class MixinV1_7VisualsMod {
 
 	@Mixin(ItemRenderer.class)
 	public static abstract class MixinItemRenderer {
-
-		@Shadow
-		protected abstract void transformFirstPersonItem(float equipProgress, float swingProgress);
-
-		@Shadow
-		private @Final Minecraft mc;
 
 		@Redirect(method = "renderItemInFirstPerson", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/ItemRenderer;transformFirstPersonItem(FF)V"))
 		public void allowUseAndSwing(ItemRenderer itemRenderer, float equipProgress, float swingProgress) {
@@ -43,10 +42,26 @@ public abstract class MixinV1_7VisualsMod {
 		@Inject(method = "doBlockTransformations", at = @At("RETURN"))
 		public void oldBlocking(CallbackInfo callback) {
 			if(V1_7VisualsMod.enabled && V1_7VisualsMod.instance.blocking) {
-				GlStateManager.scale(0.83F, 0.88F, 0.85F);
-				GlStateManager.translate(-0.3F, 0.1F, 0.0F);
+				V1_7VisualsMod.oldBlocking();
 			}
 		}
+
+		@Inject(method = "performDrinking", at = @At("HEAD"), cancellable = true)
+		public void oldDrinking(AbstractClientPlayer clientPlayer, float partialTicks, CallbackInfo callback) {
+			if(V1_7VisualsMod.enabled && V1_7VisualsMod.instance.eatingAndDrinking) {
+				callback.cancel();
+				V1_7VisualsMod.oldDrinking(itemToRender, clientPlayer, partialTicks);
+			}
+		}
+
+		@Shadow
+		protected abstract void transformFirstPersonItem(float equipProgress, float swingProgress);
+
+		@Shadow
+		private @Final Minecraft mc;
+
+		@Shadow
+		private ItemStack itemToRender;
 
 	}
 
@@ -85,7 +100,8 @@ public abstract class MixinV1_7VisualsMod {
 			return entity.getEyeHeight();
 		}
 
-		@Shadow private Minecraft mc;
+		@Shadow
+		private Minecraft mc;
 
 	}
 
--- game/src/main/java/me/mcblueparrot/client/mod/impl/V1_7VisualsMod.java
+++ game/src/main/java/me/mcblueparrot/client/mod/impl/V1_7VisualsMod.java
@@ -5,6 +5,8 @@
 
 package me.mcblueparrot.client.mod.impl;
 
+import org.lwjgl.opengl.GL11;
+
 import com.google.gson.annotations.Expose;
 
 import me.mcblueparrot.client.event.EventHandler;
@@ -15,9 +17,12 @@ import me.mcblueparrot.client.mod.Mod;
 import me.mcblueparrot.client.mod.ModCategory;
 import me.mcblueparrot.client.mod.annotation.Option;
 import me.mcblueparrot.client.util.access.AccessEntityLivingBase;
+import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.item.ItemBow;
 import net.minecraft.item.ItemFishingRod;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.MathHelper;
 import net.minecraft.util.MovingObjectPosition;
 
 public class V1_7VisualsMod extends Mod {
@@ -31,6 +36,9 @@ public class V1_7VisualsMod extends Mod {
 	@Expose
 	@Option
 	public boolean blocking = true;
+	@Expose
+	@Option
+	public boolean eatingAndDrinking = true;
 	@Expose
 	@Option
 	private boolean bow = true;
@@ -119,6 +127,30 @@ public class V1_7VisualsMod extends Mod {
 				GlStateManager.scale(0.93f, 1.0f, 1.0f);
 			}
 		}
+	}
+
+	public static void oldDrinking(ItemStack itemToRender, AbstractClientPlayer clientPlayer, float partialTicks) {
+		float var14 = clientPlayer.getItemInUseCount() - partialTicks + 1.0F;
+		float var15 = 1.0F - var14 / itemToRender.getMaxItemUseDuration();
+		float var16 = 1.0F - var15;
+		var16 = var16 * var16 * var16;
+		var16 = var16 * var16 * var16;
+		var16 = var16 * var16 * var16;
+		var16 -= 0.05F;
+		float var17 = 1.0F - var16;
+		GlStateManager.translate(0.0F, MathHelper.abs(MathHelper.cos(var14 / 4F * (float) Math.PI) * 0.1F)
+				* (float) ((double) var15 > 0.2D ? 1 : 0), 0.0F);
+		GlStateManager.translate(var17 * 0.6F, -var17 * 0.5F, 0.0F);
+		GlStateManager.rotate(var17 * 90.0F, 0.0F, 1.0F, 0.0F);
+		GlStateManager.rotate(var17 * 10.0F, 1.0F, 0.0F, 0.0F);
+		GlStateManager.rotate(var17 * 30.0F, 0.0F, 0.0F, 1.0F);
+		GlStateManager.translate(0, -0.0F, 0.06F);
+		GlStateManager.rotate(-4F, 1, 0, 0);
+	}
+		
+	public static void oldBlocking() {
+		GlStateManager.scale(0.83F, 0.88F, 0.85F);
+		GlStateManager.translate(-0.3F, 0.1F, 0.0F);
 	}
 
 }

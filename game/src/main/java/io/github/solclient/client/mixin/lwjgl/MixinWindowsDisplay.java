7f7dbab7257fbcd2a6631afbb9ac3ba8eb5eaa72
TheKodeToad <57493648+TheKodeToad@users.noreply.github.com>
2022-06-11 20:42:48 +0100

    Add remap = false for lwjgl mixin

--- game/src/main/java/io/github/solclient/client/mixin/lwjgl/MixinWindowsDisplay.java
+++ game/src/main/java/io/github/solclient/client/mixin/lwjgl/MixinWindowsDisplay.java
@@ -14,7 +14,7 @@ import net.minecraft.util.ChatComponentText;
 @Mixin(targets = "org.lwjgl.opengl.WindowsDisplay")
 public abstract class MixinWindowsDisplay {
 
-    @Inject(method = "doHandleMessage", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "doHandleMessage", at = @At("HEAD"), cancellable = true, remap = false)
     private void doHandleMessage(long hwnd, int msg, long wParam, long lParam, long millis, CallbackInfoReturnable<Long> callback) {
         if(msg == 0x020B) {
             handleMouseButton(wParam >> 16 == 1L ? 3 : 4, 1, millis);

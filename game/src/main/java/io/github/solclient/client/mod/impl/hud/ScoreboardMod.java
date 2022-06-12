bf0fbaf08454288d92c56ba3b9cb446c06ec3f50
TheKodeToad <57493648+TheKodeToad@users.noreply.github.com>
2022-06-12 10:57:21 +0100

    Scoreboard mod customisation ✏🔥 Features include: Space saving when numbers are disabled. Scaling. Finally 🥳! Suggested by kachow 🚘️ Hiding the scoreboard entirely. Aero's suggestion ✈

--- game/src/main/java/io/github/solclient/client/mod/impl/hud/ScoreboardMod.java
+++ game/src/main/java/io/github/solclient/client/mod/impl/hud/ScoreboardMod.java
@@ -8,14 +8,17 @@ import com.google.common.collect.Lists;
 import com.google.gson.annotations.Expose;
 
 import io.github.solclient.client.event.EventHandler;
+import io.github.solclient.client.event.impl.PreGameOverlayRenderEvent;
 import io.github.solclient.client.event.impl.ScoreboardRenderEvent;
 import io.github.solclient.client.mod.Mod;
 import io.github.solclient.client.mod.ModCategory;
 import io.github.solclient.client.mod.annotation.Option;
+import io.github.solclient.client.mod.annotation.Slider;
 import io.github.solclient.client.mod.hud.SimpleHudMod;
 import io.github.solclient.client.util.Utils;
 import io.github.solclient.client.util.data.Colour;
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.scoreboard.Score;
 import net.minecraft.scoreboard.ScorePlayerTeam;
 import net.minecraft.scoreboard.Scoreboard;
@@ -26,6 +29,13 @@ public class ScoreboardMod extends Mod {
 	public static ScoreboardMod instance;
 	public static boolean enabled;
 
+	@Expose
+	@Option
+	@Slider(min = 50, max = 150, step = 1, format = "sol_client.slider.percent")
+	public float scale = 100;
+	@Expose
+	@Option
+	public boolean hide;
 	@Expose
 	@Option(translationKey = SimpleHudMod.TRANSLATION_KEY)
 	public boolean background = true;
@@ -86,6 +96,19 @@ public class ScoreboardMod extends Mod {
 	public void onScoreboardRender(ScoreboardRenderEvent event) {
 		event.cancelled = true;
 
+		if(hide) {
+			return;
+		}
+
+		GlStateManager.pushMatrix();
+		GlStateManager.scale(scale / 100F, scale / 100F, scale / 100F);
+
+		int scaledWidth = event.scaledRes.getScaledWidth();
+		int scaledHeight = event.scaledRes.getScaledHeight();
+
+		scaledWidth /= scale / 100;
+		scaledHeight /= scale / 100;
+
 		Scoreboard scoreboard = event.objective.getScoreboard();
 		Collection<Score> collection = scoreboard.getSortedScores(event.objective);
 		List<Score> list = Lists.newArrayList(Iterables.filter(collection,
@@ -100,17 +123,20 @@ public class ScoreboardMod extends Mod {
 
 		int i = mc.fontRendererObj.getStringWidth(event.objective.getDisplayName());
 
-		for(Score score : collection) {
+		for (Score score : collection) {
 			ScorePlayerTeam scoreplayerteam = scoreboard.getPlayersTeam(score.getPlayerName());
-			String s = ScorePlayerTeam.formatPlayerName(scoreplayerteam, score.getPlayerName()) + ": "
-					+ EnumChatFormatting.RED + score.getScorePoints();
+			String s = ScorePlayerTeam.formatPlayerName(scoreplayerteam, score.getPlayerName());
+			if(numbers) {
+				 s +=  ": " + EnumChatFormatting.RED + score.getScorePoints();
+			}
 			i = Math.max(i, mc.fontRendererObj.getStringWidth(s));
 		}
 
 		int i1 = collection.size() * mc.fontRendererObj.FONT_HEIGHT;
-		int j1 = event.scaledRes.getScaledHeight() / 2 + i1 / 3;
+		int j1 = scaledHeight / 2 + i1 / 3;
 		int k1 = 3;
-		int l1 = event.scaledRes.getScaledWidth() - i - k1;
+		int l1 = scaledWidth - i - k1;
+
 		int j = 0;
 
 		for(Score score1 : collection) {
@@ -119,7 +145,7 @@ public class ScoreboardMod extends Mod {
 			String s1 = ScorePlayerTeam.formatPlayerName(scoreplayerteam1, score1.getPlayerName());
 			String s2 = "" + score1.getScorePoints();
 			int k = j1 - j * mc.fontRendererObj.FONT_HEIGHT;
-			int l = event.scaledRes.getScaledWidth() - k1 + 2;
+			int l = scaledWidth - k1 + 2;
 
 			if(background) {
 				Gui.drawRect(l1 - 2, k, l, k + mc.fontRendererObj.FONT_HEIGHT, backgroundColour.getValue());
@@ -146,9 +172,11 @@ public class ScoreboardMod extends Mod {
 
 		if (border) {
 			int top = ((j1 - j * mc.fontRendererObj.FONT_HEIGHT) - mc.fontRendererObj.FONT_HEIGHT) - 2;
-			Utils.drawOutline(l1 - 3, top, event.scaledRes.getScaledWidth() - k1 + 2,
+			Utils.drawOutline(l1 - 3, top, scaledWidth - k1 + 2,
 					top + mc.fontRendererObj.FONT_HEIGHT + 3 + i1, borderColour.getValue());
 		}
+
+		GlStateManager.popMatrix();
 	}
 
 	@Override

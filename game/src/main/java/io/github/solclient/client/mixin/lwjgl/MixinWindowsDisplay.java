0d322e562c144b4bf0d6fcc2283f9e8af689ed77
TheKodeToad <57493648+TheKodeToad@users.noreply.github.com>
2022-06-13 09:24:15 +0100

    Add parens. around bit shift Used in the original code, maybe not necessary.

--- game/src/main/java/io/github/solclient/client/mixin/lwjgl/MixinWindowsDisplay.java
+++ game/src/main/java/io/github/solclient/client/mixin/lwjgl/MixinWindowsDisplay.java
@@ -17,10 +17,13 @@ public abstract class MixinWindowsDisplay {
     @Inject(method = "doHandleMessage", at = @At("HEAD"), cancellable = true, remap = false)
     private void doHandleMessage(long hwnd, int msg, long wParam, long lParam, long millis, CallbackInfoReturnable<Long> callback) {
         if(msg == 0x020B) {
-            handleMouseButton(wParam >> 16 == 1L ? 3 : 4, 1, millis);
+            handleMouseButton((wParam >> 16) == 1L ? 3 : 4, 1, millis);
             callback.setReturnValue(1L);
         }
     }
+
+    @Shadow
+    private static native long defWindowProc(long hwnd, int msg, long wParam, long lParam);
 
     @Shadow
     protected abstract void handleMouseButton(int button, int state, long millis);

7d5582d3818b1dfe5749e2e7ca358cb04eff1ae5
TheKodeToad <TheKodeToad@proton.me>
2022-07-06 10:44:39 +0100

    Fix time changer affecting gameplay

--- game/src/main/java/io/github/solclient/client/mixin/MixinWorldInfo.java
+++ game/src/main/java/io/github/solclient/client/mixin/MixinWorldInfo.java
@@ -8,14 +8,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import io.github.solclient.client.Client;
 import io.github.solclient.client.event.impl.TimeEvent;
+import net.minecraft.client.Minecraft;
 import net.minecraft.world.storage.WorldInfo;
 
 @Mixin(WorldInfo.class)
 public class MixinWorldInfo {
 
+	private Minecraft mc = Minecraft.getMinecraft();
+
 	@Inject(method = "getWorldTime", at = @At("HEAD"), cancellable = true)
 	public void overrideWorldTime(CallbackInfoReturnable<Long> callback) {
-		callback.setReturnValue(Client.INSTANCE.bus.post(new TimeEvent(worldTime)).time);
+		if(mc.theWorld != null && (Object) this == mc.theWorld.getWorldInfo()) {
+			callback.setReturnValue(Client.INSTANCE.bus.post(new TimeEvent(worldTime)).time);
+		}
 	}
 
 	@Shadow

1abcc3fdf60ff8db98617080ff8d46b2e3fdae83
TheKodeToad <TheKodeToad@proton.me>
2022-09-26 17:02:11 +0100

    Hide vanilla potions when hud is enabled

---
+++ game/1.19.2/src/main/java/io/github/solclient/client/v1_19_2/mixins/mods/PotionEffectsModMixins.java
@@ -0,0 +1,26 @@
+package io.github.solclient.client.v1_19_2.mixins.mods;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import io.github.solclient.client.mod.impl.hud.PotionEffectsMod;
+import net.minecraft.client.gui.hud.InGameHud;
+
+public class PotionEffectsModMixins {
+
+}
+
+// hides existing potion effects
+@Mixin(InGameHud.class)
+class PotionEffectsModMixins$InGameHudMixin {
+
+	@Inject(method = "renderStatusEffectOverlay", at = @At("HEAD"), cancellable = true)
+	public void cancelRendering(CallbackInfo callback) {
+		if(PotionEffectsMod.INSTANCE.isEnabled()) {
+			callback.cancel();
+		}
+	}
+
+}

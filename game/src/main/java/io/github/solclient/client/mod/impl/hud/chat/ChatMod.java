012faf177acb814161684b633f8b89614016cca1
TheKodeToad <TheKodeToad@proton.me>
2022-11-02 17:40:59 +0000

    Why

--- game/src/main/java/io/github/solclient/client/mod/impl/hud/chat/ChatMod.java
+++ game/src/main/java/io/github/solclient/client/mod/impl/hud/chat/ChatMod.java
@@ -153,7 +153,7 @@ public class ChatMod extends HudMod {
 
 		if(fieldName.equals("filteredWordsContent")) {
 			filteredWords = new ArrayList<>(Arrays.asList(filteredWordsContent.split("\\r?\\n"))); // https://stackoverflow.com/a/454913
-			filteredWords.removeIf((word) -> word.isEmpty() || word.startsWith("#"));
+			filteredWords.removeIf((word) -> word.isEmpty() || word.indexOf('#') == 0);
 		}
 	}
 
@@ -242,6 +242,7 @@ public class ChatMod extends HudMod {
 		}
 
 		event.cancel();
+
 		Chat chat = mc.getIngameHud().getChat();
 
 		if(visibility == ChatVisibility.HIDDEN) {

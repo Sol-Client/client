dadf9bd2d829ebc97d2f056ffed0e64319e8325b
TheKodeToad <TheKodeToad@proton.me>
2023-01-21 11:09:32 +0000

    Wyvest made me

--- wrapper/src/main/java/io/github/solclient/client/mixin/client/MixinBuiltChunk.java
+++ wrapper/src/main/java/io/github/solclient/client/mixin/client/MixinBuiltChunk.java
@@ -6,30 +6,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import io.github.solclient.client.Client;
 import io.github.solclient.client.event.impl.RenderChunkPositionEvent;
-import io.github.solclient.util.GlobalConstants;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.VertexBuffer;
 import net.minecraft.client.world.BuiltChunk;
-import net.minecraft.util.Util;
 import net.minecraft.util.math.BlockPos;
 
 @Mixin(BuiltChunk.class)
 public class MixinBuiltChunk {
-
-	/**
-	 * @reason OptiFine for 1.8 has some issues on Linux. This fixed it, so I don't
-	 *         question it.
-	 *
-	 *         TODO this is awful. I did this before patcher had a better fix.
-	 */
-	@Redirect(method = "delete", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/VertexBuffer;delete()V"))
-	public void cancelDelete(VertexBuffer instance) {
-		if (Util.getOperatingSystem() == Util.OperatingSystem.LINUX && MinecraftClient.getInstance().options.vbo
-				&& GlobalConstants.optifine)
-			return;
-
-		instance.delete();
-	}
 
 	@Inject(method = "method_10160", at = @At("RETURN"))
 	public void setPosition(BlockPos pos, CallbackInfo callback) {
---
+++ wrapper/src/main/java/io/github/solclient/client/mixin/client/MixinVertexBuffer.java
@@ -0,0 +1,27 @@
+package io.github.solclient.client.mixin.client;
+
+import java.nio.ByteBuffer;
+
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.client.render.VertexBuffer;
+
+@Mixin(VertexBuffer.class)
+public class MixinVertexBuffer {
+
+	// Thanks Sychic!
+	// this is a much better way of doing what we were doing before
+	// https://github.com/Sk1erLLC/Patcher/pull/98
+
+	@Inject(method = "data", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/VertexBuffer;bind()V"), cancellable = true)
+	public void preventBindingToDeleted(ByteBuffer buffer, CallbackInfo callback) {
+		if (id == -1)
+			callback.cancel();
+	}
+
+	@Shadow
+	private int id;
+
+}

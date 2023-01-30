32b0812602cca5d47e5175cf83593e0361d45a76
TheKodeToad <TheKodeToad@proton.me>
2023-01-30 11:12:45 +0000

    Rewrite LCCH
    It was previously fairly closely based on Lunar's proprietary implementation.
    I did try to make it a bit more original previously, but that was with little knowledge of how it worked so it ended up looking a bit too similar to Lunar's code.
    Lunar's implementation is actually broken, but this commit fixes that!

--- wrapper/src/main/java/io/github/solclient/client/util/LCCH.java
+++ wrapper/src/main/java/io/github/solclient/client/util/LCCH.java
@@ -14,8 +14,9 @@ public class LCCH {
 
 	/**
 	 * Parse a Lunar Client crosshair and copy it to a pixel matrix.
+	 *
 	 * @param input the input string.
-	 * @param out the crosshair.
+	 * @param out   the crosshair.
 	 * @throws IllegalArgumentException if the string is not valid.
 	 */
 	public void parse(String input, PixelMatrix out) throws IllegalArgumentException {
@@ -25,37 +26,46 @@ public class LCCH {
 			throw new IllegalArgumentException("Missing prefix");
 
 		input = input.substring(input.indexOf('-') + 1);
-		if (input.indexOf('-') == -1)
-			throw new IllegalArgumentException("Missing '-'");
 
-		int srcSize = Integer.parseInt(input.substring(0, input.indexOf('-')));
-		int dstSize = out.getWidth();
+		if (input.indexOf('-') == -1)
+			throw new IllegalArgumentException("Missing data");
+
+		int size = Integer.parseInt(input.substring(0, input.indexOf('-')));
+		int offset = out.getWidth() / 2 - size / 2;
+
 		input = input.substring(input.indexOf('-') + 1);
 
-		byte[] data = Base64.getDecoder().decode(input);
-		int offset = MathHelper.floor((dstSize - srcSize) / 2F);
+		if (input.isEmpty())
+			throw new IllegalArgumentException("Data is empty");
 
 		out.clear();
-		for (int y = 0; y < srcSize; y++) {
-			for (int x = 0; x < srcSize; x++) {
-				int pixelIndex = y * srcSize + x;
-				int srcIndex = pixelIndex / 8;
-				int mask = 1 << pixelIndex % 8;
 
-				if (srcIndex < 0 || srcIndex >= data.length)
+		byte[] data = Base64.getDecoder().decode(input);
+
+		for (int y = 0; y < size; y++) {
+			for (int x = 0; x < size; x++) {
+				int srcIndex = y * size + x;
+				int srcHunk = srcIndex / 8;
+				int srcMask = 1 << srcIndex % 8;
+
+				int dstIndex = out.getIndex(x + offset, y + offset);
+
+				if (srcHunk >= data.length)
 					continue;
-				if (x + offset < 0 || x + offset >= dstSize || y + offset < 0 || x + offset >= dstSize)
+				if (dstIndex >= out.pixels())
 					continue;
 
-				if ((data[srcIndex] & mask) != 0)
-					out.set(x + offset + (y + offset) * dstSize);
+				if ((data[srcHunk] & srcMask) != 0)
+					out.set(dstIndex);
+				else
+					out.clear(dstIndex);
 			}
 		}
 	}
 
 	/**
 	 * Convert a crosshair to a Lunar Client string.
-	 * Broken but it is in Lunar too. I promise!
+	 *
 	 * @param in the pixel matrix.
 	 * @return the string.
 	 */
@@ -65,52 +75,49 @@ public class LCCH {
 		StringBuilder result = new StringBuilder();
 		result.append("LCCH-");
 
-		int srcSize = in.getWidth();
-		int dstSize = getSize(in, srcSize);
-		result.append(dstSize);
+		int size = getEffectiveSize(in);
+		int offset = in.getWidth() / 2 - size / 2;
+		result.append(size);
 		result.append('-');
 
-		int offset = MathHelper.floor((srcSize - dstSize) / 2);
-		byte[] data = new byte[MathHelper.ceil(dstSize * dstSize / 8F)];
+		byte[] data = new byte[MathHelper.ceil(size * size / 8F)];
 
-		for (int y = 0; y < dstSize; y++) {
-			for (int x = 0; x < dstSize; x++) {
-				if (!in.get(x + offset, y + offset))
-					continue;
+		for (int y = 0; y < size; y++) {
+			for (int x = 0; x < size; x++) {
+				int dstIndex = y * size + x;
+				int dstHunk = dstIndex / 8;
+				int dstMask = 1 << dstIndex % 8;
 
-				int pixelIndex = y * dstSize + x;
-				data[pixelIndex / 8] |= 1 << (pixelIndex % 8);
+				if (in.get(x + offset, y + offset))
+					data[dstHunk] |= dstMask;
 			}
 		}
 
-		result.append(new String(Base64.getEncoder().withoutPadding().encode(data)));
+		result.append(Base64.getEncoder().withoutPadding().encodeToString(data));
 		return result.toString();
 	}
 
-	private int getSize(PixelMatrix matrix, int srcSize) {
+	private int getEffectiveSize(PixelMatrix matrix) {
 		int result = 1;
-		for (int i = 0; i < matrix.pixels(); i++) {
-			if (!matrix.get(i))
-				continue;
 
-			int x = i % srcSize;
-			int y = i / srcSize;
+		for (int y = 0; y < matrix.getHeight(); y++) {
+			for (int x = 0; x < matrix.getWidth(); x++) {
+				if (!matrix.get(x, y))
+					continue;
 
-			int pxSize;
-			if ((pxSize = applyOffset(x, srcSize)) > result)
-				result = pxSize;
-			if ((pxSize = applyOffset(y, srcSize)) > result)
-				result = pxSize;
+				int pxSize = sizeFromDistance(x, y, matrix.getWidth() / 2);
+				result = Math.max(result, pxSize);
+				// it's not going to get any larger...
+				if (pxSize == matrix.getWidth())
+					break;
+			}
 		}
+
 		return result;
 	}
 
-	private int applyOffset(int value, int srcSize) {
-		value -= srcSize / 2;
-		value *= 2;
-		if (value <= 0)
-			value = -value + 1;
-		return value;
+	private int sizeFromDistance(int x, int y, int from) {
+		return Math.max(Math.abs(x - from) * 2 + 1, Math.abs(y - from) * 2 + 1);
 	}
 
 	private void validate(PixelMatrix matrix) {

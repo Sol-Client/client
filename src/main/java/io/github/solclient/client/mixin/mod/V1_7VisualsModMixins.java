4f6337fe48a34f9ac0d5c3bf84f361bf93ee572f
TheKodeToad <TheKodeToad@proton.me>
2023-03-19 16:57:30 +0000

    What happened here??

--- src/main/java/io/github/solclient/client/mixin/mod/V1_7VisualsModMixins.java
+++ src/main/java/io/github/solclient/client/mixin/mod/V1_7VisualsModMixins.java
@@ -20,6 +20,7 @@ package io.github.solclient.client.mixin.mod;
 
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.callback.*;
 
 import com.mojang.blaze3d.platform.GlStateManager;
@@ -29,6 +30,7 @@ import io.github.solclient.client.util.MinecraftUtils;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.hud.DebugHud;
+import net.minecraft.client.gui.widget.*;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.render.GameRenderer;
 import net.minecraft.client.render.entity.feature.ArmorFeatureRenderer;
@@ -57,18 +59,19 @@ public abstract class V1_7VisualsModMixins {
 		private final MinecraftClient mc = MinecraftClient.getInstance();
 
 		@Inject(method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;)V", at = @At("HEAD"))
-		public void renderItemModelForEntity(ItemStack stack, LivingEntity entity, ModelTransformation.Mode mode, CallbackInfo ci) {
+		public void renderItemModelForEntity(ItemStack stack, LivingEntity entity, ModelTransformation.Mode mode,
+				CallbackInfo ci) {
 			lastEntityToRenderFor = entity;
 		}
 
-
-		@Inject(method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/BakedModel;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;)V", at = @At(value = "INVOKE",
-				target = "Lnet/minecraft/client/render/item/ItemRenderer;renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/BakedModel;)V"))
-		public void renderItemModelForEntity_renderItem(ItemStack stack, BakedModel model, ModelTransformation.Mode transformation, CallbackInfo ci) {
-			if (V1_7VisualsMod.enabled && V1_7VisualsMod.instance.useAndMine &&
-					transformation == ModelTransformation.Mode.THIRD_PERSON && lastEntityToRenderFor instanceof PlayerEntity &&
-					mc.player.getStackInHand() != null && mc.player.getItemUseTicks() > 0 &&
-					mc.player.getStackInHand().getUseAction() == UseAction.BLOCK) {
+		@Inject(method = "renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/BakedModel;Lnet/minecraft/client/render/model/json/ModelTransformation$Mode;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/ItemRenderer;renderItem(Lnet/minecraft/item/ItemStack;Lnet/minecraft/client/render/model/BakedModel;)V"))
+		public void renderItemModelForEntity_renderItem(ItemStack stack, BakedModel model,
+				ModelTransformation.Mode transformation, CallbackInfo ci) {
+			if (V1_7VisualsMod.enabled && V1_7VisualsMod.instance.useAndMine
+					&& transformation == ModelTransformation.Mode.THIRD_PERSON
+					&& lastEntityToRenderFor instanceof PlayerEntity && mc.player.getStackInHand() != null
+					&& mc.player.getItemUseTicks() > 0
+					&& mc.player.getStackInHand().getUseAction() == UseAction.BLOCK) {
 				GlStateManager.translate(-0.041, -0.095f, 0.212f);
 				GlStateManager.rotate(80, 1.0f, 0.0f, 0.0f);
 				GlStateManager.rotate(20, 0.0f, 1.0f, 0.0f);
@@ -78,98 +81,100 @@ public abstract class V1_7VisualsModMixins {
 			}
 		}
 
-		@Mixin(HeldItemRenderer.class)
-		public static abstract class HeldItemRendererMixin {
+	}
 
-			@Shadow
-			protected abstract void applyEquipAndSwingOffset(float equipProgress, float swingProgress);
+	@Mixin(HeldItemRenderer.class)
+	public static abstract class HeldItemRendererMixin {
 
-			@Shadow
-			private @Final MinecraftClient client;
+		@Shadow
+		protected abstract void applyEquipAndSwingOffset(float equipProgress, float swingProgress);
 
-			@Shadow
-			private ItemStack mainHand;
+		@Shadow
+		private @Final MinecraftClient client;
 
-			@Redirect(method = "renderArmHoldingItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/HeldItemRenderer;applyEquipAndSwingOffset(FF)V"))
-			public void allowUseAndSwing(HeldItemRenderer instance, float equipProgress, float swingProgress) {
-				applyEquipAndSwingOffset(equipProgress,
-						swingProgress == 0.0F && V1_7VisualsMod.enabled && V1_7VisualsMod.instance.useAndMine
-								? client.player.getHandSwingProgress(MinecraftUtils.getTickDelta())
-								: swingProgress);
-			}
+		@Shadow
+		private ItemStack mainHand;
 
-			@Inject(method = "applySwordBlockTransformation", at = @At("RETURN"))
-			public void oldBlocking(CallbackInfo callback) {
-				if (V1_7VisualsMod.enabled && V1_7VisualsMod.instance.blocking) {
-					GlStateManager.scale(0.83F, 0.88F, 0.85F);
-					GlStateManager.translate(-0.3F, 0.10F, -0.01F);
-				}
-			}
+		@Redirect(method = "renderArmHoldingItem", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/item/HeldItemRenderer;applyEquipAndSwingOffset(FF)V"))
+		public void allowUseAndSwing(HeldItemRenderer instance, float equipProgress, float swingProgress) {
+			applyEquipAndSwingOffset(equipProgress,
+					swingProgress == 0.0F && V1_7VisualsMod.enabled && V1_7VisualsMod.instance.useAndMine
+							? client.player.getHandSwingProgress(MinecraftUtils.getTickDelta())
+							: swingProgress);
+		}
 
-			@Inject(method = "applyEatOrDrinkTransformation", at = @At("HEAD"), cancellable = true)
-			public void oldDrinking(AbstractClientPlayerEntity clientPlayer, float partialTicks, CallbackInfo callback) {
-				if (V1_7VisualsMod.enabled && V1_7VisualsMod.instance.eatingAndDrinking) {
-					callback.cancel();
-					V1_7VisualsMod.oldDrinking(mainHand, clientPlayer, partialTicks);
-				}
+		@Inject(method = "applySwordBlockTransformation", at = @At("RETURN"))
+		public void oldBlocking(CallbackInfo callback) {
+			if (V1_7VisualsMod.enabled && V1_7VisualsMod.instance.blocking) {
+				GlStateManager.scale(0.83F, 0.88F, 0.85F);
+				GlStateManager.translate(-0.3F, 0.10F, -0.01F);
 			}
 		}
 
-		@Mixin(GameRenderer.class)
-		public static abstract class GameRendererMixin {
+		@Inject(method = "applyEatOrDrinkTransformation", at = @At("HEAD"), cancellable = true)
+		public void oldDrinking(AbstractClientPlayerEntity clientPlayer, float partialTicks,
+				CallbackInfo callback) {
+			if (V1_7VisualsMod.enabled && V1_7VisualsMod.instance.eatingAndDrinking) {
+				callback.cancel();
+				V1_7VisualsMod.oldDrinking(mainHand, clientPlayer, partialTicks);
+			}
+		}
+	}
 
-			private float eyeHeightSubtractor;
-			private long lastEyeHeightUpdate;
+	@Mixin(GameRenderer.class)
+	public static abstract class GameRendererMixin {
 
-			@Shadow
-			private /* why you not final :( */ MinecraftClient client;
+		private float eyeHeightSubtractor;
+		private long lastEyeHeightUpdate;
 
-			// this code makes me long for spaghetti
-			@Redirect(method = "transformCamera", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;getEyeHeight()F"))
-			public float smoothSneaking(Entity entity) {
-				if (V1_7VisualsMod.enabled && V1_7VisualsMod.instance.sneaking && entity instanceof PlayerEntity) {
-					PlayerEntity player = (PlayerEntity) entity;
-					float height = player.getEyeHeight();
-					if (player.isSneaking()) {
-						height += 0.08F;
+		@Shadow
+		private /* why you not final :( */ MinecraftClient client;
+
+		// this code makes me long for spaghetti
+		@Redirect(method = "transformCamera", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;getEyeHeight()F"))
+		public float smoothSneaking(Entity entity) {
+			if (V1_7VisualsMod.enabled && V1_7VisualsMod.instance.sneaking && entity instanceof PlayerEntity) {
+				PlayerEntity player = (PlayerEntity) entity;
+				float height = player.getEyeHeight();
+				if (player.isSneaking()) {
+					height += 0.08F;
+				}
+				float actualEyeHeightSubtractor = player.isSneaking() ? 0.08F : 0;
+				long sinceLastUpdate = System.currentTimeMillis() - lastEyeHeightUpdate;
+				lastEyeHeightUpdate = System.currentTimeMillis();
+				if (actualEyeHeightSubtractor > eyeHeightSubtractor) {
+					eyeHeightSubtractor += sinceLastUpdate / 500f;
+					if (actualEyeHeightSubtractor < eyeHeightSubtractor) {
+						eyeHeightSubtractor = actualEyeHeightSubtractor;
 					}
-					float actualEyeHeightSubtractor = player.isSneaking() ? 0.08F : 0;
-					long sinceLastUpdate = System.currentTimeMillis() - lastEyeHeightUpdate;
-					lastEyeHeightUpdate = System.currentTimeMillis();
+				} else if (actualEyeHeightSubtractor < eyeHeightSubtractor) {
+					eyeHeightSubtractor -= sinceLastUpdate / 500f;
 					if (actualEyeHeightSubtractor > eyeHeightSubtractor) {
-						eyeHeightSubtractor += sinceLastUpdate / 500f;
-						if (actualEyeHeightSubtractor < eyeHeightSubtractor) {
-							eyeHeightSubtractor = actualEyeHeightSubtractor;
-						}
-					} else if (actualEyeHeightSubtractor < eyeHeightSubtractor) {
-						eyeHeightSubtractor -= sinceLastUpdate / 500f;
-						if (actualEyeHeightSubtractor > eyeHeightSubtractor) {
-							eyeHeightSubtractor = actualEyeHeightSubtractor;
-						}
+						eyeHeightSubtractor = actualEyeHeightSubtractor;
 					}
-					return height - eyeHeightSubtractor;
 				}
-				return entity.getEyeHeight();
+				return height - eyeHeightSubtractor;
 			}
+			return entity.getEyeHeight();
 		}
+	}
 
-		@Mixin(ArmorFeatureRenderer.class)
-		public static class LayerArmorBaseMixin {
+	@Mixin(ArmorFeatureRenderer.class)
+	public static class LayerArmorBaseMixin {
 
-			@Inject(method = "combineTextures", at = @At("HEAD"), cancellable = true)
-			public void oldArmour(CallbackInfoReturnable<Boolean> callback) {
-				if (V1_7VisualsMod.enabled && V1_7VisualsMod.instance.armourDamage)
-					callback.setReturnValue(true);
-			}
+		@Inject(method = "combineTextures", at = @At("HEAD"), cancellable = true)
+		public void oldArmour(CallbackInfoReturnable<Boolean> callback) {
+			if (V1_7VisualsMod.enabled && V1_7VisualsMod.instance.armourDamage)
+				callback.setReturnValue(true);
 		}
+	}
 
-		@Mixin(BiPedModel.class)
-		public static class BiPedModelMixin {
+	@Mixin(BiPedModel.class)
+	public static class BiPedModelMixin {
 
-			@ModifyConstant(method = "setAngles", constant = @Constant(floatValue = -0.5235988F))
-			private float cancelRotation(float value) {
-				return (V1_7VisualsMod.enabled && V1_7VisualsMod.instance.blocking) ? 0.0F : value;
-			}
+		@ModifyConstant(method = "setAngles", constant = @Constant(floatValue = -0.5235988F))
+		private float cancelRotation(float value) {
+			return (V1_7VisualsMod.enabled && V1_7VisualsMod.instance.blocking) ? 0.0F : value;
 		}
 	}
 
@@ -263,8 +268,10 @@ public abstract class V1_7VisualsModMixins {
 					max / 1024 / 1024);
 			text.drawWithShadow(usedString, window.getWidth() - text.getStringWidth(usedString), 2, 0xE0E0E0);
 
-			String allocatedString = String.format("Allocated memory: %d%% (%dMB)", total * 100 / max, total / 1024 / 1024);
-			text.drawWithShadow(allocatedString, window.getWidth() - text.getStringWidth(allocatedString), 12, 0xE0E0E0);
+			String allocatedString = String.format("Allocated memory: %d%% (%dMB)", total * 100 / max,
+					total / 1024 / 1024);
+			text.drawWithShadow(allocatedString, window.getWidth() - text.getStringWidth(allocatedString), 12,
+					0xE0E0E0);
 		}
 
 		@Shadow
